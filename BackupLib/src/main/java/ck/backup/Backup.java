bdff9e35dc9952661d244917f118b18480b39966
ck <chengkks1987n@126.com>
2014-08-07 21:59:37 +0800

    add comment to file src/main/java/ck/db/DBConfig.java, adjust coments for src/main/java/ck/backup/Backup.java

--- BackupLib/src/main/java/ck/backup/Backup.java
+++ BackupLib/src/main/java/ck/backup/Backup.java
@@ -7,69 +7,69 @@ import java.sql.SQLException;
 import ck.db.*;
 
 /**
- * apply the main functions of this library, such as backup, restore and search. 
- * @author ck
- * @time 2014-08-06
- */
+   apply the main functions of this library, such as backup, restore and search. 
+   @author ck &lt chengkechengke@gmail.com &gt
+   @version 2014-08-06
+*/
 public class Backup {
-	public static boolean backup(String key, String remark) {
+    public static boolean backup(String key, String remark) {
 		
-		return true;
-	}
-	public static boolean restore(String key){
-		return true;
-	}
+	return true;
+    }
+    public static boolean restore(String key){
+	return true;
+    }
 	
-	/**
-	 * search records in table src_table.
-	 * @param conditions 
-	 *  the conditions in SQL where part.
-	 * @return	
-	 *  records in the table. if the conditions is null or the size of conditions is zero,
-	 *  return all records; otherwise return the records according the conditions.		
-	*/
-	public static Iterator<SrcTable> searchSrc(Iterator<String> conditions){			
-		DBConnect db = new DBConnect();
-		db.open();
-		ResultSet rs = db.query(constructSql(conditions, "src_table"));
-		ArrayList<SrcTable> ans = new ArrayList<SrcTable>();
-		try {
-			while (rs.next()) {
-				SrcTable a = new SrcTable();
-				a.setId(rs.getString("id"));
-				a.setText(rs.getString("text"));
-				ans.add(a);
-			}
-		} catch(SQLException e) {
-			System.out.println("cannot read the records in table src_table!"); 
-			e.printStackTrace(); 			
-		}
-		db.close();	
-		return ans.iterator();
+    /**
+     * search records in table src_table.
+     * @param conditions 
+     *  the conditions in SQL where part.
+     * @return	
+     *  records in the table. if the conditions is null or the size of conditions is zero,
+     *  return all records; otherwise return the records according the conditions.		
+     */
+    public static Iterator<SrcTable> searchSrc(Iterator<String> conditions){			
+	DBConnect db = new DBConnect();
+	db.open();
+	ResultSet rs = db.query(constructSql(conditions, "src_table"));
+	ArrayList<SrcTable> ans = new ArrayList<SrcTable>();
+	try {
+	    while (rs.next()) {
+		SrcTable a = new SrcTable();
+		a.setId(rs.getString("id"));
+		a.setText(rs.getString("text"));
+		ans.add(a);
+	    }
+	} catch(SQLException e) {
+	    System.out.println("cannot read the records in table src_table!"); 
+	    e.printStackTrace(); 			
 	}
+	db.close();	
+	return ans.iterator();
+    }
 	
-	public static Iterator<DesTable> searchDes(Iterator<String> conditions){
-		return null;
-	}
+    public static Iterator<DesTable> searchDes(Iterator<String> conditions){
+	return null;
+    }
 	
-	/**
-	 * construct the SQL select language
-	 * @param conditions 
-	 *  the conditions in SQL where part.
-	 * @param tablename 
-	 *  the table to be searched
-	 * @return	
-	 *  the SQL query string. f the conditions is null or the size of conditions is zero,
-	 *  ignore the wher part.
-	*/
-	private static String constructSql(Iterator<String> conditions, String tablename) {
-		StringBuilder sql = new StringBuilder("select * from " + tablename);	
-		if (conditions.hasNext()) {
-			sql.append(" where "+conditions.next());
-		}
-		while (conditions.hasNext()) {
-			sql.append(" and "+conditions.next());
-		}	
-		return sql.toString();
+    /**
+     * construct the SQL select language
+     * @param conditions 
+     *  the conditions in SQL where part.
+     * @param tablename 
+     *  the table to be searched
+     * @return	
+     *  the SQL query string. f the conditions is null or the size of conditions is zero,
+     *  ignore the wher part.
+     */
+    private static String constructSql(Iterator<String> conditions, String tablename) {
+	StringBuilder sql = new StringBuilder("select * from " + tablename);	
+	if (conditions.hasNext()) {
+	    sql.append(" where "+conditions.next());
 	}
+	while (conditions.hasNext()) {
+	    sql.append(" and "+conditions.next());
+	}	
+	return sql.toString();
+    }
 }
--- BackupLib/src/main/java/ck/db/DBConfig.java
+++ BackupLib/src/main/java/ck/db/DBConfig.java
@@ -4,9 +4,14 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
+/**
+   @brief get the database connect configurations in the properties file.
+   @author ck &lt chengkechengke@gmail.com &gt
+   @version 2014-08-07 
+*/
 public class DBConfig {
 	
-    private static final String configFile = "database_config.properties";
+    private static final String configFile = "database_config.properties"; //!< configuration file name
     private static Properties config = new Properties();	
 		
     static {
@@ -20,18 +25,30 @@ public class DBConfig {
 	}			
     }
 	
+    /**
+       get the database driver name
+    */
     public static String getDriver(){
 	return config.getProperty("driver");			
     }
 	
+    /**
+       get the database connection URL string.
+    */
     public static String getURL(){
 	return config.getProperty("url");			
     }
 	
+    /**
+       get the user name of the database
+    */
     public static String getUsername(){
 	return config.getProperty("username");			
     }
 	
+    /**
+       get the password of the database
+    */
     public static String getPassword(){
 	return config.getProperty("password");			
     }	
